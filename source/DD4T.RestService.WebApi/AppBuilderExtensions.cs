d7e16339fd6dc2ba05e041ac1cb88fbabe7917f3
sshibani <siawash.shibani@gmail.com>
2015-09-08 09:55:49 +0200

    first commit

---
+++ source/DD4T.RestService.WebApi/AppBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using DD4T.ContentModel.Contracts.Configuration;
+using DD4T.ContentModel.Contracts.Logging;
+using DD4T.ContentModel.Contracts.Providers;
+using DD4T.ContentModel.Contracts.Resolvers;
+using DD4T.DI.Autofac;
+using DD4T.Utils;
+using DD4T.Utils.Logging;
+using DD4T.Utils.Resolver;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DD4T.RestService.WebApi
+{
+    public static class AppBuilderExtensions
+    {
+        public static void UseDD4TWebApi(this IAppBuilder appBuilder)
+        {
+           
+            var config = new HttpConfiguration();
+            var container = BuildContainer();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Always;
+            config.MapHttpAttributeRoutes();
+
+            appBuilder.UseAutofacMiddleware(container);
+            appBuilder.UseAutofacWebApi(config);
+            appBuilder.UseWebApi(config);
+          
+        }
+
+
+        static ILifetimeScope BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(typeof(AppBuilderExtensions).Assembly);
+
+            builder.UseDD4T();
+            //builder.RegisterType<DD4TConfiguration>().As<IDD4TConfiguration>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<DefaultPublicationResolver>().As<IPublicationResolver>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<DefaultLogger>().As<ILogger>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<TridionPageProvider>().As<IPageProvider>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<TridionComponentPresentationProvider>().As<IComponentPresentationProvider>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<TridionBinaryProvider>().As<IBinaryProvider>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<TridionLinkProvider>().As<ILinkProvider>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<TridionTaxonomyProvider>().As<ITaxonomyProvider>().InstancePerLifetimeScope().PreserveExistingDefaults();
+            //builder.RegisterType<ProvidersFacade>().As<IProvidersFacade>().InstancePerLifetimeScope().PreserveExistingDefaults();
+
+            return builder.Build();
+        }
+   
+    }
+}
---
+++ source/DD4T.RestService.WebApi/Controllers/BinaryController.cs
@@ -0,0 +1,153 @@
+﻿using DD4T.ContentModel.Contracts.Providers;
+using DD4T.ContentModel.Contracts.Logging;
+using DD4T.RestService.WebApi.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DD4T.RestService.WebApi.Controllers
+{
+    [RoutePrefix("binary")]
+    public class BinaryController : ApiController
+    {
+        private readonly IBinaryProvider BinaryProvider;
+        private readonly ILogger Logger;
+
+        public BinaryController(IBinaryProvider binaryProvider, ILogger logger)
+        {
+            if (binaryProvider == null)
+                throw new ArgumentNullException("binaryProvider");
+            if (logger == null)
+                throw new ArgumentNullException("logger");
+
+            Logger = logger;
+            BinaryProvider = binaryProvider;
+        }
+
+
+        [HttpGet]
+        [Route("GetBinaryByUri/{publicationId:int}/{id:int}")]
+        public IHttpActionResult GetBinaryByUri(int publicationId, int id)
+        {
+            Logger.Debug("GetBinaryByUri  publicationId={0}, componentId={1}", publicationId, id);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetBinaryByUri(id.ToComponentTcmUri(publicationId));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<byte[]>(binary);
+        }
+
+        [HttpGet]
+        [Route("GetBinaryByUrl/{publicationId:int}/{extension}/{*url}")]
+        public IHttpActionResult GetBinaryByUrl(int publicationId, string extension, string url)
+        {
+            Logger.Debug("GetBinaryByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetBinaryByUrl(url.GetUrl(extension));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<byte[]>(binary);
+        }
+
+        [HttpGet]
+        [Route("GetBinaryStreamByUri/{publicationId:int}/{id:int}")]
+        public IHttpActionResult GetBinaryStreamByUri(int publicationId, int id)
+        {
+            Logger.Debug("GetBinaryStreamByUri  publicationId={0}, componentId={1}", publicationId, id);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetBinaryStreamByUri(id.ToComponentTcmUri(publicationId));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<Stream>(binary);
+        }
+
+        [HttpGet]
+        [Route("GetBinaryStreamByUrl/{publicationId:int}/{extension}/{*url}")]
+        public IHttpActionResult GetBinaryStreamByUrl(int publicationId, string extension, string url)
+        {
+            Logger.Debug("GetBinaryStreamByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetBinaryStreamByUrl(url.GetUrl(extension));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<Stream>(binary);
+        }
+
+
+        [HttpGet]
+        [Route("GetLastPublishedDateByUri/{publicationId:int}/{id:int}")]
+        public IHttpActionResult GetLastPublishedDateByUri(int publicationId, int id)
+        {
+            Logger.Debug("GetLastPublishedDateByUri  publicationId={0}, componentId={1}", publicationId, id);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetLastPublishedDateByUri(id.ToComponentTcmUri(publicationId));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<DateTime>(binary);
+        }
+
+        [HttpGet]
+        [Route("GetLastPublishedDateByUrl/{publicationId:int}/{extension}/{*url}")]
+        public IHttpActionResult GetLastPublishedDateByUrl(int publicationId, string extension, string url)
+        {
+            Logger.Debug("GetLastPublishedDateByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetLastPublishedDateByUrl(url.GetUrl(extension));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<DateTime>(binary);
+        }
+
+
+        [HttpGet]
+        [Route("GetUrlForUri/{publicationId:int}/{id:int}")]
+        public IHttpActionResult GetUrlForUri(int publicationId, int id)
+        {
+            Logger.Debug("GetUrlForUri publicationId={0}, componentId={1}", publicationId, id);
+            if (publicationId == 0)
+                return BadRequest(Messages.EmptyPublicationId);
+
+            BinaryProvider.PublicationId = publicationId;
+            var binary = BinaryProvider.GetUrlForUri(id.ToComponentTcmUri(publicationId));
+
+            if (binary == null)
+                NotFound();
+
+            return Ok<string>(binary);
+        }
+    }
+}
