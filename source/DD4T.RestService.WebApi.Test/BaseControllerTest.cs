26eccb0fb28d4dcd7fa7130eb244e8656468b2ba
Quirijn Slings <quirijn.slings@trivident.com>
2016-03-30 12:11:18 +0200

    Added unit testing (currently only one test method, please add more)

---
+++ source/DD4T.RestService.WebApi.Test/BaseControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using DD4T.ContentModel.Contracts.Configuration;
+using DD4T.ContentModel.Contracts.Logging;
+using DD4T.ContentModel.Contracts.Providers;
+using DD4T.ContentModel.Contracts.Serializing;
+using DD4T.Providers.Test;
+using DD4T.RestService.WebApi.Controllers;
+using DD4T.Serialization;
+using DD4T.Utils.Logging;
+using Ninject;
+using Ninject.Modules;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DD4T.RestService.WebApi.Test
+{
+    public class BaseControllerTest
+    {
+        protected static BinaryController BinaryController { get; set; }
+        protected static PageController PageController { get; set; }
+        protected static ComponentPresentationController ComponentPresentationController { get; set; }
+        public static void Initialize()
+        {
+            var kernel = new StandardKernel(new RegistrationModule());
+            kernel.Load("DD4T.ContentModel.Contracts");
+            kernel.Load("DD4T.RestService.WebApi");
+            kernel.Load("DD4T.Providers.Test");
+
+            BinaryController = kernel.Get<BinaryController>();
+            PageController = kernel.Get<PageController>();
+            ComponentPresentationController = kernel.Get<ComponentPresentationController>();
+
+            BinaryController.Request = new HttpRequestMessage();
+            BinaryController.Request.SetConfiguration(new HttpConfiguration());
+        }
+
+        public class RegistrationModule : NinjectModule
+        {
+            public override void Load()
+            {
+                Bind<IDD4TConfiguration>().To<TestConfiguration>().InSingletonScope();
+                Bind<ILogger>().To<NullLogger>().InSingletonScope();
+                Bind<IProvidersCommonServices>().To<ProviderCommonServices>().InSingletonScope();
+                Bind<IPageProvider>().To<TridionPageProvider>().InSingletonScope();
+                Bind<ILinkProvider>().To<TridionLinkProvider>().InSingletonScope();
+                Bind<IBinaryProvider>().To<TridionBinaryProvider>().InSingletonScope();
+                Bind<IComponentPresentationProvider>().To<TridionComponentPresentationProvider>().InSingletonScope();
+                Bind<ISerializerService>().To<JSONSerializerService>().InSingletonScope();
+            }
+        }
+    }
+}
---
+++ source/DD4T.RestService.WebApi.Test/BinaryControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DD4T.RestService.WebApi.Controllers;
+using DD4T.ContentModel;
+using System.Web.Http.Results;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace DD4T.RestService.WebApi.Test
+{
+    [TestClass]
+    public class BinaryControllerTest : BaseControllerTest
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            Initialize();
+        }
+
+        [TestMethod]
+        public void BinaryControllerGetBinaryMetaByUrl()
+        {
+            var result = BinaryController.GetBinaryMetaByUrl(1, "png", "/media/123") as OkNegotiatedContentResult<IBinaryMeta>;
+            Assert.IsNotNull(result);
+            IBinaryMeta binaryMeta = result.Content;
+            Assert.IsNotNull(binaryMeta);         
+            Assert.AreEqual(binaryMeta.Id, "tcm:1-2");
+        }
+    }
+}
