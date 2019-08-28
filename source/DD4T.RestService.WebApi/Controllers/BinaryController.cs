5614ade527061069345149b71dda7a66c2414002
Kaan <kaan.demirkapu@incentro.com>
2019-08-28 09:45:46 +0200

    upgrade to sites9

--- source/DD4T.RestService.WebApi/Controllers/BinaryController.cs
+++ source/DD4T.RestService.WebApi/Controllers/BinaryController.cs
@@ -2,185 +2,178 @@
 using DD4T.ContentModel.Contracts.Logging;
 using DD4T.RestService.WebApi.Helpers;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using DD4T.ContentModel;
 using System.Web.Http.Description;
 
 namespace DD4T.RestService.WebApi.Controllers
 {
-    [RoutePrefix("binary")]
-    public class BinaryController : ApiController
-    {
-        private readonly IBinaryProvider BinaryProvider;
-        private readonly ILogger Logger;
+	[RoutePrefix("binary")]
+	public class BinaryController : ApiController
+	{
+		private readonly IBinaryProvider BinaryProvider;
+		private readonly ILogger Logger;
 
-        public BinaryController(IBinaryProvider binaryProvider, ILogger logger)
-        {
-            if (binaryProvider == null)
-                throw new ArgumentNullException("binaryProvider");
-            if (logger == null)
-                throw new ArgumentNullException("logger");
+		public BinaryController(IBinaryProvider binaryProvider, ILogger logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException("logger");
+			BinaryProvider = binaryProvider ?? throw new ArgumentNullException("binaryProvider");
+		}
 
-            Logger = logger;
-            BinaryProvider = binaryProvider;
-        }
+		[HttpGet]
+		[Route("GetBinaryByUri/{publicationId:int}/{id:int}")]
+		public IHttpActionResult GetBinaryByUri(int publicationId, int id)
+		{
+			Logger.Debug($"GetBinaryByUri  publicationId={publicationId}, componentId={id}");
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
+			BinaryProvider.PublicationId = publicationId;
+			var binary = BinaryProvider.GetBinaryByUri(id.ToComponentTcmUri(publicationId));
 
-        [HttpGet]
-        [Route("GetBinaryByUri/{publicationId:int}/{id:int}")]
-        public IHttpActionResult GetBinaryByUri(int publicationId, int id)
-        {
-            Logger.Debug("GetBinaryByUri  publicationId={0}, componentId={1}", publicationId, id);
-            if (publicationId == 0)
-                return BadRequest(Messages.EmptyPublicationId);
+			if (binary == null)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetBinaryByUri(id.ToComponentTcmUri(publicationId));
+			return Ok(binary);
+		}
 
-            if (binary == null)
-                NotFound();
+		[HttpGet]
+		[Route("GetBinaryByUrl/{publicationId:int}/{extension}/{*url}")]
+		public IHttpActionResult GetBinaryByUrl(int publicationId, string extension, string url)
+		{
+			Logger.Debug($"GetBinaryByUrl  publicationId={publicationId}, url={url}, extension={extension}");
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<byte[]>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var binary = BinaryProvider.GetBinaryByUrl(url.GetUrl(extension));
 
-        [HttpGet]
-        [Route("GetBinaryByUrl/{publicationId:int}/{extension}/{*url}")]
-        public IHttpActionResult GetBinaryByUrl(int publicationId, string extension, string url)
-        {
-            Logger.Debug("GetBinaryByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+			if (binary == null)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetBinaryByUrl(url.GetUrl(extension));
+			return Ok(binary);
+		}
 
-            if (binary == null)
-                NotFound();
+		[HttpGet]
+		[Route("GetBinaryStreamByUri/{publicationId:int}/{id:int}")]
+		public IHttpActionResult GetBinaryStreamByUri(int publicationId, int id)
+		{
+			Logger.Debug($"GetBinaryStreamByUri  publicationId={publicationId}, componentId={id}");
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<byte[]>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var binary = BinaryProvider.GetBinaryStreamByUri(id.ToComponentTcmUri(publicationId));
 
-        [HttpGet]
-        [Route("GetBinaryStreamByUri/{publicationId:int}/{id:int}")]
-        public IHttpActionResult GetBinaryStreamByUri(int publicationId, int id)
-        {
-            Logger.Debug("GetBinaryStreamByUri  publicationId={0}, componentId={1}", publicationId, id);
-            if (publicationId == 0)
-                return BadRequest(Messages.EmptyPublicationId);
+			if (binary == null)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetBinaryStreamByUri(id.ToComponentTcmUri(publicationId));
+			return Ok(binary);
+		}
 
-            if (binary == null)
-                NotFound();
+		[HttpGet]
+		[Route("GetBinaryStreamByUrl/{publicationId:int}/{extension}/{*url}")]
+		public IHttpActionResult GetBinaryStreamByUrl(int publicationId, string extension, string url)
+		{
+			Logger.Debug($"GetBinaryStreamByUrl  publicationId={publicationId}, url={url}, extension={extension}");
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<Stream>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var binary = BinaryProvider.GetBinaryStreamByUrl(url.GetUrl(extension));
 
-        [HttpGet]
-        [Route("GetBinaryStreamByUrl/{publicationId:int}/{extension}/{*url}")]
-        public IHttpActionResult GetBinaryStreamByUrl(int publicationId, string extension, string url)
-        {
-            Logger.Debug("GetBinaryStreamByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+			if (binary == null)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetBinaryStreamByUrl(url.GetUrl(extension));
+			return Ok(binary);
+		}
 
-            if (binary == null)
-                NotFound();
+		[Obsolete("Use GetBinaryMetaByUri method")]
+		[HttpGet]
+		[Route("GetLastPublishedDateByUri/{publicationId:int}/{id:int}")]
+		public IHttpActionResult GetLastPublishedDateByUri(int publicationId, int id)
+		{
+			Logger.Debug($"GetLastPublishedDateByUri  publicationId={publicationId}, componentId={id}");
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<Stream>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var lastPublishedDate = BinaryProvider.GetLastPublishedDateByUri(id.ToComponentTcmUri(publicationId));
 
-        [Obsolete("Use GetBinaryMetaByUri method")]
-        [HttpGet]
-        [Route("GetLastPublishedDateByUri/{publicationId:int}/{id:int}")]
-        public IHttpActionResult GetLastPublishedDateByUri(int publicationId, int id)
-        {
-            Logger.Debug("GetLastPublishedDateByUri  publicationId={0}, componentId={1}", publicationId, id);
-            if (publicationId == 0)
-                return BadRequest(Messages.EmptyPublicationId);
+			if (lastPublishedDate == DateTime.MinValue)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetLastPublishedDateByUri(id.ToComponentTcmUri(publicationId));
+			return Ok(lastPublishedDate);
+		}
 
-            if (binary == null)
-                NotFound();
+		[Obsolete("Use GetBinaryMetaByUrl method")]
+		[HttpGet]
+		[Route("GetLastPublishedDateByUrl/{publicationId:int}/{extension}/{*url}")]
+		public IHttpActionResult GetLastPublishedDateByUrl(int publicationId, string extension, string url)
+		{
+			Logger.Debug("GetLastPublishedDateByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<DateTime>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var lastPublishedDate = BinaryProvider.GetLastPublishedDateByUrl(url.GetUrl(extension));
 
-        [Obsolete("Use GetBinaryMetaByUrl method")]
-        [HttpGet]
-        [Route("GetLastPublishedDateByUrl/{publicationId:int}/{extension}/{*url}")]
-        public IHttpActionResult GetLastPublishedDateByUrl(int publicationId, string extension, string url)
-        {
-            Logger.Debug("GetLastPublishedDateByUrl  publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+			if (lastPublishedDate == DateTime.MinValue)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetLastPublishedDateByUrl(url.GetUrl(extension));
+			return Ok(lastPublishedDate);
+		}
 
-            if (binary == null)
-                NotFound();
+		[HttpGet]
+		[Route("GetBinaryMetaByUri/{publicationId:int}/{id:int}")]
+		public IHttpActionResult GetBinaryMetaByUri(int publicationId, int id)
+		{
+			Logger.Debug("GetBinaryMetaByUri publicationId={0}, componentId={1}", publicationId, id);
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<DateTime>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var binaryMeta = BinaryProvider.GetBinaryMetaByUri(id.ToComponentTcmUri(publicationId));
 
-        [HttpGet]
-        [Route("GetBinaryMetaByUri/{publicationId:int}/{id:int}")]
-        public IHttpActionResult GetBinaryMetaByUri(int publicationId, int id)
-        {
-            Logger.Debug("GetBinaryMetaByUri publicationId={0}, componentId={1}", publicationId, id);
-            if (publicationId == 0)
-                return BadRequest(Messages.EmptyPublicationId);
+			if (binaryMeta == null)
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetBinaryMetaByUri(id.ToComponentTcmUri(publicationId));
+			return Ok(binaryMeta);
+		}
 
-            if (binary == null)
-                NotFound();
+		[HttpGet]
+		[Route("GetBinaryMetaByUrl/{publicationId:int}/{extension}/{*url}")]
+		[ResponseType(typeof(BinaryMeta))]
+		public IHttpActionResult GetBinaryMetaByUrl(int publicationId, string extension, string url)
+		{
+			Logger.Debug("GetBinaryMetaByUrl publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok<IBinaryMeta>(binary);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			if (!(BinaryProvider.GetBinaryMetaByUrl(url.GetUrl(extension)) is IBinaryMeta binaryMeta))
+				return NotFound();
 
-        [HttpGet]
-        [Route("GetBinaryMetaByUrl/{publicationId:int}/{extension}/{*url}")]
-        [ResponseType(typeof(BinaryMeta))]
-        public IHttpActionResult GetBinaryMetaByUrl(int publicationId, string extension, string url)
-        {
-            Logger.Debug("GetBinaryMetaByUrl publicationId={0}, url={1}, extension={2}", publicationId, url, extension);
-            
-            BinaryProvider.PublicationId = publicationId;
-            IBinaryMeta binaryMeta = BinaryProvider.GetBinaryMetaByUrl(url.GetUrl(extension)) as IBinaryMeta;
-                      
-            if (binaryMeta == null)
-            {
-                return NotFound();
-            }
-            Logger.Debug($"about to return binarymeta {binaryMeta.Id}");
+			Logger.Debug($"about to return binarymeta {binaryMeta.Id}");
+			return Ok(binaryMeta);
+		}
 
+		[HttpGet]
+		[Route("GetUrlForUri/{publicationId:int}/{id:int}")]
+		public IHttpActionResult GetUrlForUri(int publicationId, int id)
+		{
+			Logger.Debug("GetUrlForUri publicationId={0}, componentId={1}", publicationId, id);
+			if (publicationId <= 0)
+				return BadRequest(Messages.EmptyPublicationId);
 
-            return Ok(binaryMeta);
-        }
+			BinaryProvider.PublicationId = publicationId;
+			var url = BinaryProvider.GetUrlForUri(id.ToComponentTcmUri(publicationId));
 
-        [HttpGet]
-        [Route("GetUrlForUri/{publicationId:int}/{id:int}")]
-        public IHttpActionResult GetUrlForUri(int publicationId, int id)
-        {
-            Logger.Debug("GetUrlForUri publicationId={0}, componentId={1}", publicationId, id);
-            if (publicationId == 0)
-                return BadRequest(Messages.EmptyPublicationId);
+			if (string.IsNullOrEmpty(url))
+				return NotFound();
 
-            BinaryProvider.PublicationId = publicationId;
-            var binary = BinaryProvider.GetUrlForUri(id.ToComponentTcmUri(publicationId));
-
-            if (binary == null)
-                NotFound();
-
-            return Ok<string>(binary);
-        }
-    }
+			return Ok(url);
+		}
+	}
 }
